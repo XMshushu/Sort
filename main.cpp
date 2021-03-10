36d54f1a1e41ed930487ea1f94cae8ca0e65d6f1
XMshushu <2403430873@qq.com>
2021-03-11 06:17:30 +0800

    选择排序

--- main.cpp
+++ main.cpp
@@ -4,3 +4,17 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
+//选择排序
+void selectSort(int R[],int n){
+    int i,j,k;
+    int tmp;
+    for(i=0;i<n;++i){
+        k=i;
+        for(j=i+1;j<n;++j)
+            if(R[i]>R[j])
+                k=j;
+        tmp=R[j];
+        R[j]=R[i];
+        R[i]=tmp;
+    }
+}
